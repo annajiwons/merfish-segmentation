3783bd852f320f97b8272b4596bc8eeba28a6c95
Anna Song <annajiwons@gmail.com>
2021-01-27 15:23:53 -0800

    refactor everything else

---
+++ 3_splinedist_merfish_manual_seg/train.py
@@ -0,0 +1,54 @@
+import sys
+sys.path.append('../')
+sys.path.append('../splinedistm')
+
+from csbdeep.utils import normalize
+from glob import glob
+from skimage import io
+from splinedist import fill_label_holes
+from splinedist.models import Config2D, SplineDist2D
+from splinedist.utils import phi_generator, grid_generator, get_contoursize_max
+from tqdm import tqdm
+
+from utils import train_validation_split
+
+image_dir = f'../images'
+merfish_dir = f'{image_dir}/merfish'
+
+model_dir = './models'
+
+# Read train images that manually segmentated and fill small label holes
+merfish_X_train_names = sorted(glob(f'{merfish_dir}/train/images/*.tif'))
+merfish_X_train = [normalize(img, 1, 99.8, axis=(0, 1)) for img in list(map(io.imread, merfish_X_train_names))]
+merfish_Y_train_names = sorted(glob(f'{merfish_dir}/train/masks/manual/*.tif'))
+merfish_Y_train = [fill_label_holes(img) for img in tqdm(list(map(io.imread, merfish_Y_train_names)))]
+
+# Train validation split
+merfish_X_train, merfish_Y_train, merfish_X_valid, merfish_Y_valid = train_validation_split(merfish_X_train, merfish_Y_train, 0.1)
+
+# Using configuration from SplineDist example
+# choose the number of control points (M)
+M = 6
+n_params = 2 * M
+
+# Predict on subsampled grid for increased efficiency and larger field of view
+grid = (4,4)
+
+# compute the size of the largest contour present in the image-set
+contoursize_max = get_contoursize_max(merfish_Y_train)
+
+conf = Config2D (
+    n_params        = n_params,
+    grid            = grid,
+    n_channel_in    = 1,
+    contoursize_max = contoursize_max,
+    train_patch_size = (512,512),
+)
+print(conf)
+vars(conf)
+
+phi_generator(M, conf.contoursize_max)
+grid_generator(M, conf.train_patch_size, conf.grid)
+
+splinedist_merfish_model = SplineDist2D(conf, name='splinedist_merfish', basedir=model_dir)
+splinedist_merfish_model.train(merfish_X_train, merfish_Y_train, validation_data=(merfish_X_valid, merfish_Y_valid), augmenter=None, epochs=300)
---
+++ 3_stardist_merfish_manual_seg/train.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.append('../')
+
+from csbdeep.utils import normalize
+from glob import glob
+from skimage import io
+from stardist import fill_label_holes, random_label_cmap
+from stardist.matching import matching_dataset
+from stardist.models import Config2D, StarDist2D
+from tqdm import tqdm
+
+from utils import train_validation_split
+
+image_dir = f'../images'
+merfish_dir = f'{image_dir}/merfish'
+
+model_dir = './models'
+
+# Read train images that manually segmentated and fill small label holes
+merfish_X_manual_train_names = sorted(glob(f'{merfish_dir}/train/images/*.tif'))
+merfish_X_manual_train = [normalize(img, 1, 99.8, axis=(0, 1)) for img in list(map(io.imread, merfish_X_manual_train_names))]
+merfish_Y_manual_train_names = sorted(glob(f'{merfish_dir}/train/masks/manual/*.tif'))
+merfish_Y_manual_train = [fill_label_holes(img) for img in tqdm(list(map(io.imread, merfish_Y_manual_train_names)))]
+
+# Train validation split
+merfish_X_manual_train, merfish_Y_manual_train, merfish_X_manual_valid, merfish_Y_manual_valid = train_validation_split(merfish_X_manual_train, merfish_Y_manual_train)
+
+conf = Config2D (
+    n_rays       = 32,
+    grid         = (2,2),
+    n_channel_in = 1,
+)
+print(conf)
+vars(conf)
+
+# Train StarDist model
+manual_mask_merfish_model = StarDist2D(conf, name='manual_mask_merfish_model', basedir=model_dir)
+manual_mask_merfish_model.train(merfish_X_manual_train, merfish_Y_manual_train, validation_data=(merfish_X_manual_valid, merfish_Y_manual_valid), augmenter=None, epochs=300)
