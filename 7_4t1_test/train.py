4bbfce539f3197a766c954b341d946425fb8bb3b
Anna Song <annajiwons@gmail.com>
2021-03-19 02:24:29 +0000

    train model with additional 4t1 images

--- 7_4t1_test/train.py
+++ 7_4t1_test/train.py
@@ -19,7 +19,7 @@ model_dir = './models'
 # Read train images that manually segmentated and fill small label holes
 X_train_names = sorted(glob(f'{nuclei_4t1_dir}/train/images/*.tif'))
 X_train = [normalize(img, 1, 99.8, axis=(0, 1)) for img in list(map(io.imread, X_train_names))]
-Y_train_names = sorted(glob(f'{nuclei_4t1_dir}/train/masks/manual/*.tif'))
+Y_train_names = sorted(glob(f'{nuclei_4t1_dir}/train/masks/*.tif'))
 Y_train = [fill_label_holes(img) for img in tqdm(list(map(io.imread, Y_train_names)))]
 
 # Train validation split
@@ -34,7 +34,8 @@ print(conf)
 vars(conf)
 
 # Load pretrained StarDist model trained with 2d fluorescent images + 10 additional u2os images
-pretrained_stardist_model = StarDist2D(None, name = '2D_versatile_fluo_10_u2os', basedir = model_dir) 
+# Train with all additional images
+pretrained_stardist_model = StarDist2D(None, name = '2D_versatile_fluo_10_u2os_5', basedir = model_dir) 
 pretrained_stardist_model.train(X_train, Y_train, 
                                 validation_data=(X_valid, Y_valid), 
                                 augmenter=None, epochs=300)
